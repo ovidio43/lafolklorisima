052fa61a61c81cc84734df3c1578fc4d7b442df0
edwin <ovidio43@gmail.com>
2014-04-04 06:15:29 -0400

    sa

--- wp-content/themes/themeLafolklorisma_v2/index.php
+++ wp-content/themes/themeLafolklorisma_v2/index.php
@@ -1,61 +1,8 @@
 <?php get_header();?>
 
-<div class="wrapper">
-  <div class="row article">
-  <?php
-  $args=array(
-    'post_type' => "fotos",
-    'post_status' => 'publish',
-    'orderby' => 'date',
-    'order' => 'DESC',
-    'posts_per_page'=>2);                 
-  $myposts = new WP_Query( $args );  
-  if ( $myposts->have_posts() ) :
-    while ( $myposts->have_posts() ) : $myposts->the_post(); ?>
-      <div class="col-md-3 entry-thumbnail">
-          <?php 
-           if ( has_post_thumbnail()) { ?>
-           <a href="<?php the_permalink();?>">
-           <?php
-             echo get_the_post_thumbnail($post->ID, 'crop_thumbnail'); ?>
-             <span class="icon-"></span>
-             </a>
-           <?php }
-          ?>
-          <h2 class="entry-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-      </div>      
-    <?php endwhile;
-  endif;
-  ?>
-  <?php
-  $args=array(
-    'post_type' => "videos",
-    'post_status' => 'publish',
-    'orderby' => 'date',
-    'order' => 'DESC',
-    'posts_per_page'=>2);                 
-  $myposts = new WP_Query( $args );  
-  if ( $myposts->have_posts() ) :
-    while ( $myposts->have_posts() ) : $myposts->the_post(); ?>
-      <div class="col-md-3 entry-thumbnail">
-          <?php 
-           if ( has_post_thumbnail()) { ?>
-           <a href="<?php the_permalink();?>">
-           <?php
-             echo get_the_post_thumbnail($post->ID, 'crop_thumbnail'); ?>
-             <span class="icon-"></span>
-             </a>
-           <?php }
-          ?>
-          <h2 class="entry-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-      </div>      
-    <?php endwhile;
-  endif;
-  ?>  
-  </div>
-</div>
 
 <?php
+$type=array('post','fotos','videos');
 $args=array(
     'post_type' => $type,
     'post_status' => 'publish',
@@ -70,10 +17,14 @@ $args=array(
           <div class="row">
             <div class="entry_image col-md-8">
               <?php 
+                $type=get_post_type();
+                if($type=="post")
+                $type=get_post_format();
                if ( has_post_thumbnail()) {
                  echo get_the_post_thumbnail(get_the_ID(), 'featured-image'); 
                }
-              ?>            
+              ?>
+              <span class="icon-<?php echo $type;?>"></span>            
             </div>
             <div class="entry_content col-md-4">
               <h1 class="entry-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h1>
@@ -89,6 +40,7 @@ $args=array(
 <div class="wrapper">
   <div class="row article">
   <?php
+  $type=array('post','fotos','videos');
   $args=array(
     'post_type' => $type,
     'post_status' => 'publish',
@@ -99,14 +51,17 @@ $args=array(
   $myposts = new WP_Query( $args );  
   if ( $myposts->have_posts() ) :
     while ( $myposts->have_posts() ) : $myposts->the_post(); ?>
-      <div class="col-md-4 entry-thumbnail">
+      <div class="col-md-3 entry-thumbnail">
           <?php 
            if ( has_post_thumbnail()) { ?>
            <a href="<?php the_permalink();?>">
            <?php
-             echo get_the_post_thumbnail($post->ID, 'crop_thumbnail'); ?>
-             <span class="icon-"></span>
-             </a>
+            $type=get_post_type();
+            if($type=="post")
+            $type=get_post_format();
+            echo get_the_post_thumbnail($post->ID, 'crop_thumbnail'); ?>
+            <span class="icon-<?php echo $type;?>"></span>
+            </a>
            <?php }
           ?>
           <h2 class="entry-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
--- wp-content/themes/themeLafolklorisma_v2/library/post-type.php
+++ wp-content/themes/themeLafolklorisma_v2/library/post-type.php
@@ -33,3 +33,27 @@ function custom_post_type_init() {
 
 add_action('init', 'custom_post_type_init');
 //Defines custom image sizes used on the katy perry site.
+// create taxonomy
+add_action( 'init', 'create_my_taxonomies', 0 );
+function create_my_taxonomies() {
+    $taxonomies = array(
+        array("name_tax" => "musica-categoria", "related_tax" => "musica", "name" => "Categoria Musica", "add_new_item" => "Agregar Nueva Categoria", "new_item_name" => "Nueva Categoria"),
+        array("name_tax" => "musica-grupo", "related_tax" => "musica", "name" => "Agrupacion", "add_new_item" => "Agregar Nueva Agrupacion", "new_item_name" => "Nueva Agrupacion")
+    );
+    foreach ($taxonomies as $tax) {
+        register_taxonomy(
+            $tax["name_tax"],
+            $tax["related_tax"],
+            array(
+                'labels' => array(
+                    'name' => $tax["name"],
+                    'add_new_item' => $tax["add_new_item"],
+                    'new_item_name' => $tax["new_item_name"]
+                ),
+                'show_ui' => true,
+                'show_tagcloud' => false,
+                'hierarchical' => true
+            )
+        );
+    }
+}
